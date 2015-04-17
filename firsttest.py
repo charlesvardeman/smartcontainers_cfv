576552d993e378269297ad25a6d8d399d70b9c9f
dahuo2013 <dahuo2013@gmail.com>
2015-04-17 10:57:48 -0400

    updated dockertest

--- firsttest.py
+++ firsttest.py
@@ -1,5 +1,7 @@
 import provmodified as prov
 import docker
+import json
+from pprint import pprint
 
 
 docker.bind_ns("test", "http://tw.rpi.edu/ns/test#")
@@ -11,3 +13,7 @@ image.set_label("example image")
 #activity.set_label("example activity")
 
 print(docker.serialize(format="turtle"))
+
+with open('centos.json') as data_file:    
+    data = json.load(data_file)
+pprint(data)
