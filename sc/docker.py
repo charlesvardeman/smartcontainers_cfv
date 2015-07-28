79699ee4a60aa7737ca6b78d8e52ea34049df4cd
charlesvardeman <charles.vardeman@gmail.com>
2015-07-28 09:18:20 -0400

    Flushing out class structure for docker class.

---
+++ sc/docker.py
@@ -0,0 +1,21 @@
+
+
+class Docker:
+    def __init__(self,command,image)
+        self.command = command
+        self.image = image
+
+    def sanity_check(self):
+        pass
+
+    def find_docker(self):
+        pass
+
+    def do_command(self):
+        pass
+
+    def capture_command(self):
+        line = self.command
+        
+    def capture_cmd_workflow(self):
+        pass
