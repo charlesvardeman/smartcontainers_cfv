b0f424022b82bbdaaaa9a1b87b5f2f5948740623
charlesvardeman <charles.vardeman@gmail.com>
2015-07-29 14:32:49 -0400

    Adding missing util and functions to compare docker versions.

--- sc/docker.py
+++ sc/docker.py
@@ -1,4 +1,7 @@
 from util import which
+from sarge import get_stdout
+
+min_docker_version = '1.6.0'
 
 class Error(Exception):
     """Base class for docker module exceptions"""
@@ -26,11 +29,21 @@ class Docker:
             location = self.find_docker()
             if location is None:
                 raise DockerNotFoundError
+        self.check_docker_version()
 
     def find_docker(self):
         """find_docker searches paths and common directores to find docker."""
         self.location = which("docker")
         return self.location
+
+    def check_docker_version(self):
+        output = get_stdout('docker --version')
+        # in docker 1.7.1 version is at 2 position in returned string
+        version = output.split()[2]
+        # remove comma from out put if in string
+        if ',' in version:
+            version = version[:-1]
+        assert self.ver_cmp(version, min_docker_version) > 0
 
     def do_command(self):
         pass
@@ -41,3 +54,12 @@ class Docker:
 
     def capture_cmd_workflow(self):
         pass
+
+    # Function to compare sematic versioning
+    # See http://zxq9.com/archives/797 for explaination
+    # and http://semver.org/ for information on semantic versioning
+    def ver_tuple(self, z):
+        return tuple([int(x) for x in z.split('.') if x.isdigit()])
+
+    def ver_cmp(self, a, b):
+        return cmp(self.ver_tuple(a), self.ver_tuple(b))
