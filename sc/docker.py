aca4600c94a8df6734772eee4a3e6f8a6d5672db
charlesvardeman <charles.vardeman@gmail.com>
2015-07-29 12:35:14 -0400

    Adding exceptions for docker not found.

--- sc/docker.py
+++ sc/docker.py
@@ -1,21 +1,43 @@
+from util import which
 
+class Error(Exception):
+    """Base class for docker module exceptions"""
+    pass
+
+class DockerNotFoundError(RuntimeError):
+    """Exception raised for missing docker command.
+
+    Attributes:
+        msg -- explaination of error
+    """
+
+    def __init__(self):
+        msg = "Please make sure docker is installed and in your path."
+        self.arg = msg
 
 class Docker:
-    def __init__(self, command, image):
+    def __init__(self, command):
         self.command = command
-        self.image = image
+        self.location = None
 
     def sanity_check(self):
-        pass
+        """sanity_check checks existence and executability of docker."""
+        if self.location is None:
+            location = self.find_docker()
+            if location is None:
+                raise DockerNotFoundError
 
     def find_docker(self):
-        pass
+        """find_docker searches paths and common directores to find docker."""
+        self.location = which("docker")
+        return self.location
 
     def do_command(self):
         pass
 
     def capture_command(self):
         line = self.command
+        return line
 
     def capture_cmd_workflow(self):
         pass
