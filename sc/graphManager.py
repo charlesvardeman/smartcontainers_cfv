5c525a92258f9b77869705a633cdedb237a27b25
charlesvardeman <charles.vardeman@gmail.com>
2015-11-11 09:56:43 -0500

    Adding more comments to turtle file.

--- sc/graphManager.py
+++ sc/graphManager.py
@@ -1,7 +1,24 @@
 import rdflib
 from rdflib import RDF, Literal, URIRef
 
-class graphManager:
+class graphPluginRegistry(type):
+    plugins = []
+    global_graph = rdflib.ConjunctiveGraph()
+    def __init__(cls, name, bases, attrs):
+        if name != 'graphPlugin':
+            graphPluginRegistry.plugins.append(cls)
+
+class graphPlugin(object, metaclass=GraphPluginRegistry):
+    def __init__(self, post=None, db=None):
+        """ Initialize the plugin. Optinally provide the db.Post that is
+            being processed and the db.DB it belongs to.
+        """
+        self.post = post
+        self.db = db
+
+
+
+class graphPlugin:
 	"""docstring for """
 	def __init__(self):
 		# initial graphManager
